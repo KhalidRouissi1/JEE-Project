4f6c9bcb33da7ba5bfac192b6eac90f7cd1e37b6
khaled <khalidrouissi7@gmail.com>
2024-07-27 12:03:24 +0100

    First push

---
+++ src/main/java/com/example/testing/kodysimpson/XMLServlet.java
@@ -0,0 +1,15 @@
+package com.example.testing.kodysimpson;
+
+import java.io.*;
+
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+@WebServlet(name="poop")
+public class XMLServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    }
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    }
+
+}
